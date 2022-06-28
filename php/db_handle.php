1c249a64bec3efaee10b49f91d7fd913548791cb
cristianh <crishernandez08@hotmail.com>
2022-06-28 16:37:31 -0500

    ADD: comentarios php y variables css

--- php/db_handle.php
+++ php/db_handle.php
@@ -14,18 +14,22 @@ $connection = mysqli_connect($db_hostname, $db_username, $db_password, $db_name,
 mysqli_set_charset($connection, "utf8");
 
 /**
- * Funcion para registrar usuarios nuevos en la base de datos
+ * Funcion para registrar usuarios nuevos en la base de datos.
+ * @param $username STRING 
+ * @param $lastname STRING
+ * @param $email STRING
+ * @param $password STRING
  */
-function register($username, $lastname, $email, $password) {
+function register($username, $lastname, $email, $password)
+{
     global $connection;
-    if (user_exists($email)){
+    if (user_exists($email)) {
         show_alert('El correo ya se encuentra registrado');
         header("Location: http://localhost/Amigaap-v0.2/pages/register.html");
         exit();
-    }
-    else {
+    } else {
         $query = 'INSERT INTO usuarios (nombre_usuario, apellido_usuario, correo_usuario, password_usuario)
-                  VALUES ("'.$username.'","'.$lastname.'", "'.$email.'", "'.$password.'");';
+                  VALUES ("' . $username . '","' . $lastname . '", "' . $email . '", "' . $password . '");';
         mysqli_query($connection, $query);
         show_alert(mysqli_insert_id($connection));
         header("Location: http://localhost/Amigaap-v0.2/pages/login.html");
@@ -34,50 +38,58 @@ function register($username, $lastname, $email, $password) {
 }
 
 /**
- * Función para validar si un correo ya se encuentra registrado en la base de datos
+ * Función para validar si un correo ya se encuentra registrado en la base de datos.
+ * @param $email STRING
  */
-function user_exists($email) {
+function user_exists($email)
+{
     global $connection;
     $query = 'SELECT correo_usuario FROM usuarios WHERE correo_usuario = "' . $email . '";';
     $result = mysqli_query($connection, $query);
     //mysqli_fetch_assoc sirve para devolver un arreglo con los resultados obtenidos del mysqli
     if (mysqli_fetch_assoc($result)) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
 
 /**
- * Funcion para mostrar mensajes en un alert
+ * Funcion para mostrar mensajes en un alert.
+ * @param $message STRING
  */
-function show_alert($message) {
+function show_alert($message)
+{
     echo '<script> alert("' . $message . '");</script>';
 }
 
 /**
- * Funcion para loguearse
+ * Funcion para loguearse.
+ * @param $user_email STRING 
+ * @param $user_password STRING
  */
-function login($user_email, $user_password){
-    if (user_exists($user_email)){
+function login($user_email, $user_password)
+{
+    if (user_exists($user_email)) {
         validate_password($user_email, $user_password);
     }
 }
 
 /**
  * Funcion encargada de validar que la contraseña es la correcta
+ * @param $user_email STRING 
+ * @param $user_password STRING
  */
-function validate_password($user_email, $user_password) {
+function validate_password($user_email, $user_password)
+{
     global $connection;
-    $query = 'SELECT * FROM usuarios WHERE correo_usuario = "'. $user_email . '";';
+    $query = 'SELECT * FROM usuarios WHERE correo_usuario = "' . $user_email . '";';
     $result = mysqli_query($connection, $query);
     $user = mysqli_fetch_assoc($result);
     if ($user && $user['password_usuario'] == $user_password) {
         header("Location: http://localhost/Amigaap-v0.2/pages/home.html");
         exit();
-    }
-    else {
+    } else {
         show_alert('Verificar los datos ingresados, correo o contraseña incorrectos!');
         header("Location: http://localhost/Amigaap-v0.2/pages/login.html");
         exit();
